80ed2e1aba3c19ec53cae00835ab889c58a39dba
Learnatric <82117263+Learnatric@users.noreply.github.com>
2021-08-04 08:45:58 -0400

    Add files via upload

---
+++ lesson-videos-s3.js
@@ -0,0 +1,8 @@
+
+
+
+k.1.1a.i_do.mp4 : "https://learnatric-lesson-videos.s3.amazonaws.com/kindergarten/k.1.1a.i_do.mp4"
+k.1.1b.we_do1.mp4 : "https://learnatric-lesson-videos.s3.amazonaws.com/kindergarten/k.1.1b.we_do1.mp4"
+k.1.1c.we_do2.mp4 : "https://learnatric-lesson-videos.s3.amazonaws.com/kindergarten/k.1.1c.we_do2.mp4"
+k.1.1d.we_do3.mp4 : "https://learnatric-lesson-videos.s3.amazonaws.com/kindergarten/k.1.1d.we_do3.mp4"
+
